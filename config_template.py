1da98a7b35c88bf438b3c0247c35f14bf7426501
Mikhail Krylov <m.krylov@innopolis.university>
2025-07-16 18:31:03 +0300

    Changing default SMTP_PORT to 587

--- config_template.py
+++ config_template.py
@@ -8,7 +8,7 @@ db_password="9tMFDmqkbaB3"
 db_port=5432
 
 SMTP_HOST = "smtp.mail.ru"
-SMTP_PORT = 465  # для SSL
+SMTP_PORT = 587  # для SSL
 SMTP_USER = "andreyilin03@mail.ru"
 SMTP_PASSWORD = "UT397BpgFo82s1YRC8kt"
 
